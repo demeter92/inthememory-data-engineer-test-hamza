f063845b984f72a40bd80b8b1c41636b99f8e577
hamzah <hamza.hachani@outlook.com>
2025-04-27 11:57:35 +0200

    read from azure for  transactions

--- src/scripts/utils/match_transactions_files.py
+++ src/scripts/utils/match_transactions_files.py
@@ -1,38 +1,28 @@
-import os
-import re
 from typing import Optional, List
+from azure.storage.blob import ContainerClient
+from config.spark_config import ACCOUNT_NAME, CONTAINER_NAME, ACCOUNT_KEY
 
-def list_matching_transaction_files(base_path: str, date: Optional[str] = None, hour: Optional[str] = None) -> List[str]:
-    """
-    List transaction CSV files matching the pattern transactions_{date}_{hour}.csv
-    from a given base directory.
+def list_matching_transaction_files_azure(
+    date: Optional[str] = None,
+    hour: Optional[str] = None
+) -> List[str]:
+   
+    connection_string = f"DefaultEndpointsProtocol=https;AccountName={ACCOUNT_NAME};AccountKey={ACCOUNT_KEY};EndpointSuffix=core.windows.net"
+    container_client = ContainerClient.from_connection_string(
+        conn_str=connection_string,
+        container_name=CONTAINER_NAME
+    )
 
-    Args:
-        base_path (str): Local directory path (e.g., "/app/shared_storage/data/").
-        date (str, optional): Specific date in 'YYYY-MM-DD' format. If None, matches all dates.
-        hour (str, optional): Specific hour in 'H' format (e.g., '8'). If None, matches all hours.
-
-    Returns:
-        List[str]: List of full file paths prefixed with 'file://'.
-    """
-    # List all files in the folder
-    all_files = [f for f in os.listdir(base_path) if os.path.isfile(os.path.join(base_path, f))]
-    
     if date and hour:
-        # If both date and hour are given, adapt the pattern
-        pattern = re.compile(rf"transactions_{re.escape(date)}_{re.escape(hour)}\.csv")
+        # Filter for files with both date and hour
+        name_starts_with = f"transactions_{date}_{hour}"
     elif date:
-        # If only date is given, match all hours for that date
-        pattern = re.compile(rf"transactions_{re.escape(date)}_\d+\.csv")
+        # Filter for files with only the date
+        name_starts_with = f"transactions_{date}"
     else:
-        # If no date is given, match all transactions files
-        pattern = re.compile(r"transactions_\d{4}-\d{2}-\d{2}_\d+\.csv")
-    
-    # Filter files matching the desired pattern
-    matching_files = [f for f in all_files if pattern.match(f)]
-    
-    # Build full 'file://' paths
-    full_paths = [f"file://{os.path.join(base_path, f)}" for f in matching_files]
-    
-    return full_paths
-    
+        # If no date is provided, match all files with the format transactions_{date isoformat}_{hour}
+        name_starts_with = "transactions_"
+    blobs = container_client.list_blobs(name_starts_with=name_starts_with)    
+    base_url= f"wasbs://{CONTAINER_NAME}@{ACCOUNT_NAME}.blob.core.windows.net/"
+    result = [(base_url + blob.name) for blob in blobs]
+    return result
